f9e400951125f46f1178b9cd4da1b5e6a336f524
Karthi Meivel <karthi.meivel@syncfusion.com>
2026-07-08 17:22:19 +0530

    client url code changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,10 @@ import spritesRouter from "./routes/sprites.js";
 const app = express();
 const PORT = Number(process.env.PORT ?? 5000);
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
 app.use(express.json({ limit: "5mb" }));
 
 app.get("/", (_req: Request, res: Response) => {
