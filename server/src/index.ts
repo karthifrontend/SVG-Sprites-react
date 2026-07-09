120c5caee0d38ea4d43532746dcc0bb1d26934d3
Karthi Meivel <karthi.meivel@syncfusion.com>
2026-07-09 11:01:28 +0530

    revert  server cors changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,11 +7,7 @@ import spritesRouter from "./routes/sprites.js";
 const app = express();
 const PORT = Number(process.env.PORT ?? 5000);
 
-app.use(cors({
-    origin: ['https://svg-sprites-react.vercel.app'],
-    methods: ['POST', 'GET'],
-    credentials: true,
-}))
+app.use(cors())
 app.use(express.json({ limit: "5mb" }));
 
 // Log all incoming requests
